50aae4617de37a27df313b90f25db7b3f33c400f
Sanghamitra Samal <samalsanghamitra3@gmail.com>
2025-09-03 23:28:40 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 
-// const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderNest'
 const dbUrl = process.env.ATLASDB_URL;
 
 main().then(() => {
@@ -45,7 +44,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     crypto: {
-        secret: "mysupersecretcode",
+        secret: process.env.SECRET,
     },
     touchAfter: 24*3600,
 });
@@ -56,7 +55,7 @@ store.on("error", () => {
 
 const sessionOptions = {
     store,
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -65,10 +64,6 @@ const sessionOptions = {
         httpOnly: true,
     }
 };
-
-// app.get("/", (req, res) => {
-//     res.send("home page");
-// });
 
 
 app.use(session(sessionOptions));
